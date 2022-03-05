f26c55cac6a5bc563eeb544f30229a5cbf21662a
002TDG744 <Maulikkumar.Dodia@ibm.com>
2022-03-05 20:29:33 +0530

    Added Bottom Navigation Screens/Fragments

---
+++ app/src/main/java/com/waterdrinkreminder/ui/fragment/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.waterdrinkreminder.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.waterdrinkreminder.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+}
---
+++ app/src/main/java/com/waterdrinkreminder/ui/fragment/InfoFragment.kt
@@ -0,0 +1,7 @@
+package com.waterdrinkreminder.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.waterdrinkreminder.R
+
+class InfoFragment: Fragment(R.layout.fragment_info) {
+}
