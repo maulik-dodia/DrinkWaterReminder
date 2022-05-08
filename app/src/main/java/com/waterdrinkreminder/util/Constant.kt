797d579ca15b1f328cdb5bdca9289847d1c64dad
002TDG744 <Maulikkumar.Dodia@ibm.com>
2022-05-08 17:22:37 +0530

    Added ViewModel in HomeFragment

--- app/src/main/java/com/waterdrinkreminder/util/Constant.kt
+++ app/src/main/java/com/waterdrinkreminder/util/Constant.kt
@@ -2,4 +2,9 @@ package com.waterdrinkreminder.util
 
 object Constant {
     const val TWO_THOUSAND_LONG = 1000L
+
+    const val FIFTY_INT = 50
+    const val ONE_HUNDRED_INT = 100
+    const val ONE_HUNDRED_FIFTY_INT = 150
+    const val TWO_HUNDRED_INT = 200
 }
