8fadb409ee0a3619960a7ea4afc7d53f855b8f6c
002TDG744 <Maulikkumar.Dodia@ibm.com>
2022-05-09 15:58:55 +0530

    Added ViewModel in HomeFragment

--- app/src/main/java/com/waterdrinkreminder/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/waterdrinkreminder/ui/fragment/HomeFragment.kt
@@ -6,19 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.waterdrinkreminder.R
 import com.waterdrinkreminder.databinding.FragmentHomeBinding
 import com.waterdrinkreminder.util.Constant.FIFTY_INT
 import com.waterdrinkreminder.util.Constant.ONE_HUNDRED_FIFTY_INT
 import com.waterdrinkreminder.util.Constant.ONE_HUNDRED_INT
 import com.waterdrinkreminder.util.Constant.TWO_HUNDRED_INT
+import com.waterdrinkreminder.viewmodel.HomeFagViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
+
+    private val homeViewModel: HomeFagViewModel by viewModels()
 
     private lateinit var dataBinding: FragmentHomeBinding
-
-    private var totalWaterDrank = 1050
-    private var totalWaterDrinkTarget = 2070F
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,35 +32,53 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
-
-        calculatePercent(0)
-
-        dataBinding.btn50Ml.setOnClickListener {
-            calculatePercent(FIFTY_INT)
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                homeViewModel.apply {
+                    calculatePercent(0)
+                    stateFlowTotalWaterDrankPercent.collectLatest { percent ->
+                        dataBinding.tvTotalWaterDrankPercent.text =
+                            getString(R.string.placeholder_percentage, percent)
+                    }
+                }
+            }
         }
-
-        dataBinding.btn100Ml.setOnClickListener {
-            calculatePercent(ONE_HUNDRED_INT)
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                homeViewModel.stateFlowTotalWaterDrank.collectLatest { totalWaterDrank ->
+                    dataBinding.tvTotalWaterDrank.text = totalWaterDrank.toString()
+                }
+            }
         }
-
-        dataBinding.btn150Ml.setOnClickListener {
-            calculatePercent(ONE_HUNDRED_FIFTY_INT)
-        }
-
-        dataBinding.btn200Ml.setOnClickListener {
-            calculatePercent(TWO_HUNDRED_INT)
-        }
-
+        setonClickListener()
         return dataBinding.root
     }
 
-    private fun calculatePercent(milliLitresToAdd: Int) {
-        totalWaterDrank += milliLitresToAdd
-        val totalWaterDrankPercent = (totalWaterDrank / totalWaterDrinkTarget) * 100
+    private fun setonClickListener() {
         dataBinding.apply {
-            tvTotalWaterDrank.text = totalWaterDrank.toString()
-            tvTotalWaterDrankPercent.text =
-                getString(R.string.placeholder_percentage, totalWaterDrankPercent.toFloat())
+            btn50Ml.setOnClickListener(this@HomeFragment)
+            btn100Ml.setOnClickListener(this@HomeFragment)
+            btn150Ml.setOnClickListener(this@HomeFragment)
+            btn200Ml.setOnClickListener(this@HomeFragment)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        v?.let {
+            when (it.id) {
+                dataBinding.btn50Ml.id -> {
+                    homeViewModel.calculatePercent(FIFTY_INT)
+                }
+                dataBinding.btn100Ml.id -> {
+                    homeViewModel.calculatePercent(ONE_HUNDRED_INT)
+                }
+                dataBinding.btn150Ml.id -> {
+                    homeViewModel.calculatePercent(ONE_HUNDRED_FIFTY_INT)
+                }
+                dataBinding.btn200Ml.id -> {
+                    homeViewModel.calculatePercent(TWO_HUNDRED_INT)
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/waterdrinkreminder/viewmodel/HomeFagViewModel.kt
@@ -0,0 +1,22 @@
+package com.waterdrinkreminder.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class HomeFagViewModel : ViewModel() {
+
+    private var totalWaterDrinkTarget = 2070F
+
+    private val _stateFlowTotalWaterDrank = MutableStateFlow(1050)
+    val stateFlowTotalWaterDrank = _stateFlowTotalWaterDrank.asStateFlow()
+
+    private val _stateFlowTotalWaterDrankPercent = MutableStateFlow(0F)
+    val stateFlowTotalWaterDrankPercent = _stateFlowTotalWaterDrankPercent.asStateFlow()
+
+    fun calculatePercent(milliLitresToAdd: Int) {
+        _stateFlowTotalWaterDrank.value += milliLitresToAdd
+        val totalWaterDrankPercent = (_stateFlowTotalWaterDrank.value / totalWaterDrinkTarget) * 100
+        _stateFlowTotalWaterDrankPercent.value = totalWaterDrankPercent
+    }
+}
